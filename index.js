1a02bdb1cb43d4dcf840c04423ad07fb9902e140
cawilson1 <30571659+cawilson1@users.noreply.github.com>
2021-07-09 14:59:29 -0400

    remove extras

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 require("dotenv").config();
-console.log(process.env.DB_SERVER);
-const express = require("express");
-const cors = require("cors");
-const sql = require("mssql");
+const express = require('express')
+const cors = require('cors')
 
 const app = express();
-const PORT = process.env.PORT;
-const apiPrefix = process.env.API_PREFIX;
-const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
-  db: process.env.DB,
-  options: {
-   encrypt: true
-  }
-}
+const PORT = 6789;
+const apiPrefix = "/test";
+
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-sql.connect(config)
-  .then(pool => {
 
-app.get(`/${apiPrefix}/`, (request, response) => {
+app.get(`/${apiPrefix}/healthcheck`, (request, response) => {
   try {
     response.status(200).send({ message: `Hello from ${apiPrefix}` });
   } catch (error) {
@@ -31,42 +20,5 @@ app.get(`/${apiPrefix}/`, (request, response) => {
   }
 });
 
-app.post(`/${apiPrefix}/`, (request, response) => {
-  try {
-    response
-      .status(200)
-      .send({ message: `Hello from ${apiPrefix} post request` });
-  } catch (error) {
-    response.status(500).send();
-  }
-});
+app.listen(PORT, () =>console.log(`${apiPrefix} express js app is listening on ${PORT}`));
 
-app.get(`/${apiPrefix}/query`, (request, response) => {
-  try {
-    response.status(200).send({
-      message: `Hello from ${apiPrefix}. Your query was ${JSON.stringify(
-        request.query
-      )}`,
-    });
-  } catch (error) {
-    response.status(500).send();
-  }
-});
-
-app.post(`/${apiPrefix}/body`, (request, response) => {
-  try {
-    response.status(200).send({
-      message: `Hello from ${apiPrefix} post request. Your request body was ${JSON.stringify(
-        request.body
-      )}`,
-    });
-  } catch (error) {
-    response.status(500).send();
-  }
-});
-
-app.listen(PORT, () =>
-  console.log(`${apiPrefix} express js app is listening on ${PORT}`)
-);
-})
- .catch(error => console.log(error))
